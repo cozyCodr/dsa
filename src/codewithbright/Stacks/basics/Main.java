6a473cb2724587709ced990fe0ae4b88498696b9
Bright Londa <66696421+cozyCodr@users.noreply.github.com>
2022-09-01 19:02:43 +0200

    implemented in-built stack solution

--- src/codewithbright/Stacks/basics/Main.java
+++ src/codewithbright/Stacks/basics/Main.java
@@ -1,15 +1,34 @@
 package codewithbright.Stacks.basics;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args){
-        Stack<Integer> stack = new Stack<>();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        System.out.println(stack);
-        var top = stack.pop();
-        System.out.println(top);
+
+        /* **** Reversing String with inbuilt stack (my solution) **** */
+
+        String name = "Bright";
+        char[] nameArray = name.toCharArray();
+        int nameLength = nameArray.length;
+        Stack<Character> stack = new Stack<>();
+
+        // Add to stack
+        for(char i: nameArray){
+            stack.push(i);
+        }
+        // Reset name array
+        nameArray = new char[nameLength];
+
+        // get reversed string
+        for (int x = 0; x < nameLength; x++){
+            nameArray[x] = stack.pop();
+        }
+
+        // convert array to string
+        name = String.valueOf(nameArray);
+        System.out.println(name);
+        /* *************************************************** */
+
     }
 }
