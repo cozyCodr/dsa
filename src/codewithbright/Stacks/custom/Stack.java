252c7742ada31261ea8a91668d1d4633d40d664c
Bright Londa <66696421+cozyCodr@users.noreply.github.com>
2022-09-04 20:37:05 +0200

    created custom stack class

--- src/codewithbright/Stacks/custom/Stack.java
+++ src/codewithbright/Stacks/custom/Stack.java
@@ -1,2 +1,8 @@
-package codewithbright.Stacks.custom;public class Stack {
+package codewithbright.Stacks.custom;
+
+public class Stack {
+    private final int size = 10;
+    private int [] stack = new int [size];
+
+
 }
